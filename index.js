e81313c6761c73a748efeb257dc756ccf3db2227
Mageba4life <andriesmageba107@gmail.com>
2026-07-26 15:50:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 import pkg from "@whiskeysockets/baileys";
-import qrcode from "qrcode-terminal";
 import pino from "pino";
 
 const { default: makeWASocket, useMultiFileAuthState } = pkg;
 
 async function startBot() {
+
   const { state, saveCreds } = await useMultiFileAuthState("session");
 
   const sock = makeWASocket({
     auth: state,
-    logger: pino({ level: "silent" }),
-    printQRInTerminal: true
+    logger: pino({ level: "silent" })
   });
+
+  if (!sock.authState?.creds?.registered) {
+    const phoneNumber = "27792530518"; // put your number here
+    const code = await sock.requestPairingCode(phoneNumber);
+    console.log("PAIRING CODE:", code);
+  }
 
   sock.ev.on("creds.update", saveCreds);
 
-  sock.ev.on("connection.update", (update) => {
-    const { connection } = update;
+  sock.ev.on("connection.update", ({ connection }) => {
 
     if (connection === "open") {
       console.log("✅ MagebaBot connected!");
@@ -26,6 +30,7 @@ async function startBot() {
       console.log("❌ Connection closed");
       startBot();
     }
+
   });
 }
 
